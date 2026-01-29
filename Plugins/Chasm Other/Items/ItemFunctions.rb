fad418cf6285f1656bf86c63c7af8d743efcb2b1
ZinniaGameDev <xeuorux@gmail.com>
2026-01-29 16:51:15 -0700

    Fix instances of growth_rate.max_level

--- Plugins/Chasm Other/Items/ItemFunctions.rb
+++ Plugins/Chasm Other/Items/ItemFunctions.rb
@@ -577,7 +577,7 @@ end
 def pbEXPAdditionItem(pkmn, exp, item, scene = nil, oneAtATime = false)
     current_lvl = pkmn.level
     current_exp = pkmn.exp
-    level_cap = LEVEL_CAPS_USED ? getLevelCap : growth_rate.max_level
+    level_cap = LEVEL_CAPS_USED ? getLevelCap : MAX_LEVEL_CAP
 
     # Do nothing if the pokemon's already at the level cap
     if pkmn.level >= level_cap
