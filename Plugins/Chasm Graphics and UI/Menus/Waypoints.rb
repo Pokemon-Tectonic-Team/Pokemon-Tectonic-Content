72d44e2a6e8c74f4abc23f8a6f404d2457a76ac6
AlphaKretin <deewhydeeecks@gmail.com>
2026-06-01 09:14:49 +0930

    Avatar totems show fancy animation when activated (#378)

--- Plugins/Chasm Graphics and UI/Menus/Waypoints.rb
+++ Plugins/Chasm Graphics and UI/Menus/Waypoints.rb
@@ -161,6 +161,39 @@ class WaypointsTracker
 		
 		unless @activeWayPoints.has_key?(waypointName)
 			pbMessage(waypointRegisterMessage)
+
+			totemGlowSprite = $scene.spriteset.getAnimationForEvent(waypointEvent.id)
+
+			totemGlowSprite.switchAnimationMode(2)
+
+			# Pause for fading
+			loop do
+				Graphics.update
+				Input.update
+				pbUpdateSceneMap
+				break if totemGlowSprite.animationComplete?
+			end
+
+			totemGlowSprite.switchAnimationMode(1)
+
+			# Pause for cool animation
+			framesWaited = 0
+			loop do
+				Graphics.update
+				Input.update
+				# Intentionally duplicated
+				Graphics.update
+				Input.update
+				pbUpdateSceneMap
+				pbSEPlay("Totem activation") if framesWaited == 2
+				framesWaited += 1
+				break if totemGlowSprite.animationComplete?
+			end
+
+			totemGlowSprite.switchAnimationMode(0)
+
+			pbWait(10)
+			
 			addWaypoint(waypointName,waypointEvent)
 
             checkForWaypointsAchievement
@@ -380,7 +413,7 @@ def waypointAccessMessageAlternative
 end
 
 def waypointRegisterMessage
-    return _INTL("\\i[SPANNINGBAND]The Spanning Bands glow in sync with the totem. You sense that some sort of connection has been created.")
+    return _INTL("\\i[SPANNINGBAND]The Spanning Bands glow in sync with the totem.")
 end
 
 def waypointUnableMessage
