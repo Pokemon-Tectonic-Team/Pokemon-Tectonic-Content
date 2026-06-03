2535e664d52d84a0d470cc809205763e9ad27694
ShadowKirbae <toni1009.tn@gmail.com>
2026-06-03 16:29:20 +0200

    Fixed AI overvaluing overhealing in the face of
    FieldOfLife Xerneas, causing the AI to basically spam healing moves because at 100% hp it thinks it's missing 100% hp.
    Fixes #427

--- Plugins/Chasm Battle/Battler/Battler_AI_Helpers.rb
+++ Plugins/Chasm Battle/Battler/Battler_AI_Helpers.rb
@@ -62,10 +62,14 @@ class PokeBattle_Battler
         return asleep? && getStatusCount(:SLEEP) > 1
     end
 
+    OVERHEAL_AI_WEIGHT = 0.25
+
     def getHealingEffectScore(healingAmount)
-        healingScore = 0
-        healingScore += healingAmount * pbDefense
-        healingScore += healingAmount * pbSpDef
+        normalMissing = [@totalhp - @hp, 0].max
+        normalHeal = [healingAmount, normalMissing].min
+        overhealHeal = [healingAmount - normalHeal, 0].max
+        effectiveHealing = normalHeal + overhealHeal * OVERHEAL_AI_WEIGHT
+        healingScore = effectiveHealing * (pbDefense + pbSpDef)
         healingScore /= (3 * level)
         healingPercentage = (100 * healingAmount / @totalhp.to_f).round(1)
         echoln("\t\t[EFFECT SCORING] #{pbThis} scores the value of healing #{healingAmount} HP (#{healingPercentage} percent) at #{healingScore}")
