bca61276f931863ac233249792059cfe4943f387
RiptideHub <66923704+RiptideHub@users.noreply.github.com>
2026-07-23 19:36:53 -0700

    HEAVYDUTYHOOVES nerf
    30% damage boost to kicking moves -> 15%

--- Plugins/Chasm Battle/Abilities/Ability Event Handlers/Damage/DamageCalcUserAbilities.rb
+++ Plugins/Chasm Battle/Abilities/Ability Event Handlers/Damage/DamageCalcUserAbilities.rb
@@ -286,7 +286,14 @@ BattleHandlers::DamageCalcUserAbility.add(:IRONHEEL,
   }
 )
 
-BattleHandlers::DamageCalcUserAbility.copy(:IRONHEEL, :HEAVYDUTYHOOVES)
+BattleHandlers::DamageCalcUserAbility.add(:HEAVYDUTYHOOVES,
+  proc { |ability, user, target, move, mults, _baseDmg, type, aiCheck|
+    if move.kickingMove?
+      mults[:base_damage_multiplier] *= 23 / 20.0
+      user.aiLearnsAbility(ability) unless aiCheck
+    end
+  }
+)
 
 BattleHandlers::DamageCalcUserAbility.add(:BEAMING,
   proc { |ability, user, target, move, mults, _baseDmg, type, aiCheck|
