fe3a2ed9c139199de3cf4993fcf50704e802aaa4
ShadowKirbae <toni1009.tn@gmail.com>
2026-07-24 16:14:08 +0200

    Clarifying extra sandstorm ability curse effect

--- Plugins/Chasm Battle/AI/Trainers AI/Curses/SandAndNewAbilities.rb
+++ Plugins/Chasm Battle/AI/Trainers AI/Curses/SandAndNewAbilities.rb
@@ -2,7 +2,7 @@ PokeBattle_Battle::BattleStartApplyCurse.add(:CURSE_SAND_ABILITIES,
     proc { |curse_policy, battle, curses_array|
         battle.amuletActivates(
             _INTL("A Sky Scoured of Star and Sun"),
-            _INTL("The battle begins with sandstorm. The foe has extra sandstorm abilities!")
+            _INTL("The battle begins with sandstorm. Enemy Pokémon have extra sandstorm abilities!")
         )
         curses_array.push(curse_policy)
         battle.pbStartWeather(nil, :Sandstorm)
